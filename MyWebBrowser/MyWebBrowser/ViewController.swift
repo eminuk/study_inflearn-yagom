e4185c880d276296c635d191095052fcc9c14d95
이민욱 <eminuk@genieworks.net>
2020-02-10 14:05:34 +0900

    학습: 1번 프로젝트 - 웹브라우저
    UI 구성하기

---
+++ MyWebBrowser/MyWebBrowser/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyWebBrowser
+//
+//  Created by 이민욱 on 2020/02/10.
+//  Copyright © 2020 FishinGom. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
