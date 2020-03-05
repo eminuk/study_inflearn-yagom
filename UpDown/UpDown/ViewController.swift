0da562e3e16f2c626682ee3247dac66e712591a9
Minuk Lee <eminuk@naver.com>
2020-03-05 17:54:54 +0900

    학습: 3번 프로젝트 - 업다운
    프로젝트 생성
    인터페이스 구성하기
    예제 함께하기 - IBOutlet
    예제 함께하기 - IBAction
    예제 함께하기 - Gusture Recognizer
    예제 함께하기 - 메서드 코드 채우기

--- UpDown/UpDown/ViewController.swift
+++ UpDown/UpDown/ViewController.swift
@@ -10,26 +10,73 @@ import UIKit
 
 class ViewController: UIViewController {
     // MARK: - Properties
-    // MARK: - IBOutlets
+    // MARK: IBOutlets
     @IBOutlet var resultLable: UILabel!
     @IBOutlet var turnCountLabel: UILabel!
     @IBOutlet var inputField: UITextField!
     
+    // MARK: Stored Properties
+    var randomNumber: UInt32 = 0
+    var turnCount: Int = 0
+    
     // MARK: - Methods
     @IBAction func touchUpSubmitButton(_ sender: UIButton) {
         print("touch up submit button")
+        
+        guard let inputText = self.inputField.text, inputText.isEmpty == false else {
+            print("입력값 없음")
+            return
+        }
+        
+        guard let inputNumber: UInt32 = UInt32(inputText) else {
+            print("입력값이 잘못 되었음")
+            return
+        }
+        
+        turnCount += 1
+        self.turnCountLabel.text = "\(turnCount)"
+        
+        if inputNumber > randomNumber {
+            self.resultLable.text = "UP!"
+        } else if inputNumber < randomNumber {
+            self.resultLable.text = "DOWN!"
+        } else {
+            self.resultLable.text = "정답입니다!"
+        }
     }
     @IBAction func touchUpResetButton(_ sender: UIButton) {
         print("touch up reset button")
+        
+        self.initializeGame()
     }
     @IBAction func tapBackground(_ sender: UITapGestureRecognizer) {
         print("tap background view")
+        
         inputField.resignFirstResponder()
+        
+        // self.view.endEditing(true)
+        // self.inputField.resignFirstResponder()
+        // self.inputField.endEditing(true)
+    }
+    
+    // MARK: Custom Methods
+    func initializeGame(){
+        
+        self.randomNumber = arc4random() % 25
+        self.turnCount = 0
+        
+        self.resultLable.text = "Start!"
+        self.turnCountLabel.text = "\(turnCount)"
+        self.inputField.text = nil
+        
+        print("임의의 숫자 \(self.randomNumber)")
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        self.initializeGame()
     }
 
 
