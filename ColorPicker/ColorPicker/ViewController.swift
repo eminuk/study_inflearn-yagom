aff65bb469bee536d6db0a9fcd5e33e36e6e5f8f
Minuk Lee <eminuk@naver.com>
2020-03-09 14:33:04 +0900

    학습: 4번 프로젝트 - 컬러픽커
    프로젝트 생성
    인터페이스 구성하기
    아웃렛과 액션 연결

--- ColorPicker/ColorPicker/ViewController.swift
+++ ColorPicker/ColorPicker/ViewController.swift
@@ -9,7 +9,16 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var colorView: UIView!
+    @IBOutlet weak var pickerView: UIPickerView!
+    
+    
+    @IBAction func silderValueChanged(_ sender: UISlider) {
+    }
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
